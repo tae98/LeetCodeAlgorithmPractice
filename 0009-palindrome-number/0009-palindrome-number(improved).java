73ee0fb4e6ad304e6f498f2d6fa60bdf0b2f886a
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2024-10-19 19:34:16 +0900

    Time: 4 ms (100%), Space: 44.2 MB (27.72%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number(improved).java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // if x is negative or x is not zero and x ends with 0
+        if (x<0) return false;
+        if (x != 0 && x % 10 == 0) return false;
+
+        int reverse =0;
+        while(x > reverse){
+            int lastDigit = x % 10;
+            //append reversed digit 
+            reverse = (reverse * 10) + lastDigit;
+            //to move to next digit
+            x = x / 10;
+        }
+        // when x has even number legnth (ex.1221) || when x has odd number length(ex.12321)
+        return (x == reverse) || (x == reverse / 10);
+    }
+}
