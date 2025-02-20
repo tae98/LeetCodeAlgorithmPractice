5541aab47759211dcb4e241566b5c7573d147457
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-02-20 17:50:53 +0900

    Time: 1 ms (99.55%), Space: 45.2 MB (58.63%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        ArrayList<String> tmp = new ArrayList<>();
-
-        for(int i =1; i <= n; i++){
+        List<String> arr = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
             if(i % 3 == 0 && i % 5 ==0){
-                tmp.add("FizzBuzz");
-            }else if(i % 3 ==0 && i % 5 !=0){
-                tmp.add("Fizz");
-            }else if(i % 5 ==0 && i % 3 !=0){
-                tmp.add("Buzz");
+                arr.add("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                arr.add("Fizz");
+            }
+            else if(i % 5 ==0){
+                arr.add("Buzz");
             }else{
-                tmp.add(String.valueOf(i));
+                arr.add(String.valueOf(i));
             }
         }
-        return tmp;
+        return arr;
     }
 }
