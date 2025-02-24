7cdacf47b5de1a6d3669d4651447cefdc3b7d58f
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-02-24 23:32:28 +0900

    Time: 9 ms (6.46%), Space: 40.5 MB (77.81%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,11 +1,20 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int cnt =0;
-        int currPowerOfFive =5;
-        while(n >= currPowerOfFive){
-            cnt += (n / currPowerOfFive);
-            currPowerOfFive *=  5;
+        if(n == 0){
+            return 0;
         }
-        return cnt;
+
+        int ans =0;
+        int tmp = 5;
+        int pow = 1;
+        for(int i = 1; i <= n; i++){
+            if(i / tmp == 5){
+                tmp = i;
+                ans += n/tmp + 1;
+            }else if(i % 5 == 0){
+                ans += 1;
+            }
+        }
+        return ans;
     }
 }
