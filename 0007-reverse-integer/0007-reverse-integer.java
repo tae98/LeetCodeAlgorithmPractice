b11597d6a4279ebe27deb27853620853d4698cff
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-02-25 22:11:14 +0900

    Time: 1 ms (84.01%), Space: 41 MB (56.08%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        if (x == min || x == max){
+            return 0;
+        }
+        int rev = 0;
+        while(x != 0){
+            int tmp = x % 10;
+            if(rev > max/10 || rev < min/10){
+                return 0;
+            }
+            rev = (rev * 10) + tmp;
+            x = x/10;
+        }
+        return rev;
+    }
+}
