27913cb681f31d3b3a3726a587a9f9a08b9ae1f3
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-02-28 18:27:34 +0900

    Time: 0 ms (100%), Space: 42.9 MB (24.1%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,23 +1,17 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans =1;
-        long num = Math.abs((long) n);
-
-        if(x == 1){
-            return 1;
-        }
-
-        while(num !=0){
-            if (num % 2 == 1){// if n is odd num
+        long num = Math.abs((long)n);
+        double ans = 1;
+        while(num != 0){
+            if(num % 2 == 1){
                 ans = ans * x;
-                num = num-1;
+                num = num - 1;
             }
             x = x * x;
-            num = num/2;
+            num = num / 2;
         }
-
         if(n < 0){
-            ans = 1/ ans;
+            ans = 1 / ans;
         }
         return ans;
     }
