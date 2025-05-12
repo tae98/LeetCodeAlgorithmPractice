c7bce94d92cca5f9a82da8f4237804c57547fc29
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-05-12 17:09:16 +0900

    Time: 5 ms (52.81%), Space: 44.8 MB (68.65%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -10,14 +10,14 @@ class Solution {
         roman.put('D', 500);
         roman.put('M', 1000);
     
-        for(int i =0; i < s.length() -1 ; i++){
+        for(int i =0; i < s.length() -1; i++){
             if(roman.get(s.charAt(i)) < roman.get(s.charAt(i+1))){
                 ans -= roman.get(s.charAt(i));
             }else{
-                ans += roman.get(s.charAt(i));
+                ans+= roman.get(s.charAt(i));
             }
         }
-        
-        return ans + roman.get(s.charAt(s.length()-1));
+        ans += roman.get(s.charAt(s.length()-1));
+        return ans;
     }
 }
