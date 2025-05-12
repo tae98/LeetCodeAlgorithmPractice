8a6fbef21cfe80a9641a987658530264c2e1cfc5
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-05-12 17:49:26 +0900

    Time: 4 ms (42.84%), Space: 45 MB (17.25%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -8,50 +8,49 @@ class Solution {
         roman.put(100, 'C');
         roman.put(500, 'D');
         roman.put(1000, 'M');
-        String ans ="";
-
+        StringBuilder ans = new StringBuilder();
         while(num != 0){
             if( num >= 1000){
-                ans += 'M';
+                ans.append('M');
                 num -= 1000;
             }else if(num >= 900){
-                ans += "CM";
+                ans.append("CM");
                 num -= 900;
             }else if(num >= 500){
-                ans += 'D';
+                ans.append('D');
                 num -= 500;
             }else if(num >= 400){
-                ans += "CD";
+                ans.append("CD");
                 num -= 400;
             }else if(num >= 100){
-                ans += 'C';
+                ans.append('C');
                 num -= 100;
             }else if(num >= 90){
-                ans += "XC";
+                ans.append("XC");
                 num -= 90;
             }else if(num >= 50){
-                ans += 'L';
+                ans.append('L');
                 num -= 50;
             }else if(num >= 40){
-                ans += "XL";
+               ans.append("XL");
                 num -= 40;
             }else if(num >= 10){
-                ans += 'X';
+                ans.append('X');
                 num -= 10;
             }else if(num >= 9){
-                ans += "IX";
+               ans.append("IX");
                 num -= 9;
             }else if(num >= 5){
-                ans += 'V';
+                ans.append('V');
                 num -= 5;
             }else if(num >=4){
-                ans += "IV";
+                ans.append("IV");
                 num -= 4;
             }else if(num >= 1){
-                ans += 'I';
+                ans.append('I');
                 num -= 1;
             }
         }
-        return ans;
+        return ans.toString();
     }
 }
