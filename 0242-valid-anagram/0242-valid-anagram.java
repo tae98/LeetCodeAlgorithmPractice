fa039a70a1f7f5eaa69bb5ae708f3da0c3c87731
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-05-16 16:04:20 +0900

    Time: 6 ms (44.7%), Space: 43.3 MB (72.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
         if(s.length() != t.length()){
             return false;
         }
-       Map<Character, Integer> mapS = new HashMap<>();
-       Map<Character, Integer> mapT = new HashMap<>();
+        int[] cnt = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            cnt[s.charAt(i) - 'a']++;
+            cnt[t.charAt(i) - 'a']--;
+        }
 
-       for(int i = 0; i < s.length(); i++){
-        char cs = s.charAt(i);
-        char ct = t.charAt(i);
-        //getOrDefault 값이 존재 하면 값을가져옴
-        //없을 시 원하는값으로 초기값 설정 
-        mapS.put(cs, mapS.getOrDefault(cs, 0) + 1);
-        mapT.put(ct, mapT.getOrDefault(ct, 0) +1);
-       }
-       
-       return mapS.equals(mapT);
+        for(int each : cnt){
+            if(each != 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
