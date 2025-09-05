b5c359bc9cf21fdf9aeda384017e2a62d42448eb
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2025-09-05 15:44:03 +0900

    Time: 0 ms (100%), Space: 56.3 MB (60.14%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,16 +1,14 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int left = 0, right = arr.length - 1;
+        int left = 0, right = arr.length -1;
         while(left < right){
-            int mid = left + (right - left)/2;
-            // 피크는 mid 혹은 그 왼쪽에 있음
-            if(arr[mid] > arr[mid+1]){
-                right =  mid;
-            // 피크는 무조건 mid 오른쪽에 있음
-            }else if(arr[mid] < arr[mid+1]){
+            int mid = left + (right - left) / 2;
+            if(arr[mid] < arr[mid + 1]){
                 left = mid + 1;
+            }else{
+                right = mid;
             }
         }
-        return left; // 또는 right (같은 값)
+        return left;
     }
 }
