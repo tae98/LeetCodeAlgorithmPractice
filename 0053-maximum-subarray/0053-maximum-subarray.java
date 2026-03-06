0a3c0780217f5ff16a432d372e77da8d2e657d63
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2026-03-06 14:55:05 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,8 +5,8 @@ class Solution {
         int total = 0;
         int max = nums[0];
 
-        for(int i = 0; i < nums.length; i++){
-            total += nums[i];
+        for(int num : nums){
+            total += num;
             max = Math.max(max , total);
             if(total < 0){
                 total = 0;
