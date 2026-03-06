5da8b832f88e486088407be03c57810a5fdd1d72
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2026-03-06 13:08:27 +0900

    Time: 0 ms (100%), Space: 47.9 MB (96.83%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,14 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-        //s.length/2 for loop 
-        //save left on tmp
-        //let left = right 
-        //give back left's original value to right using tmp (right = tmp)
-        int len = s.length;
-        for(int i =0; i < len/2; i++){
-            char tmp = s[i];
-            s[i] = s[len - 1 -i]; //s[i] = left, s[len - 1 -i] = right
-            s[len -1 -i] = tmp;
+        int left = 0, right = s.length - 1;
+
+        while(left < right){
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
         }
     }
 }
