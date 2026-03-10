184405d99559bd30bffcb3a8ecb3c939f0e1473f
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2026-03-10 14:36:04 +0900

    Time: 0 ms (100%), Space: 43.6 MB (59.17%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,17 +1,13 @@
-import java.util.*;
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int answer = nums.length;
-        int cnt =0;
-        
-        for(int i =0; i< nums.length; i++){
+        int ans = 0, idx = 0;
+        for(int i = 0; i < nums.length; i++){
             if(nums[i] != val){
-                nums[cnt] = nums[i];
-                cnt++;
-            }else{
-                answer--;
+                nums[idx] = nums[i];
+                idx++;
+                ans++;
             }
         }
-        return answer;
+        return ans;
     }
 }
