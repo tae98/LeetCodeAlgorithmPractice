da684a43e1e28590b3c911d021ffb4d5dfaf5073
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2026-03-11 06:51:30 +0900

    Time: 5 ms (81.94%), Space: 77.6 MB (9.95%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,24 +1,19 @@
 class Solution {
     public int maxArea(int[] height) {
         int left = 0, right = height.length - 1;
-        int best = 0;
-
-        while (left < right) {
-            // 현재 후보 넓이
+        int maxA = 0;
+        int minH = Integer.MAX_VALUE;
+        while(left < right){
+            minH = Math.min(height[left], height[right]);
             int width = right - left;
-            int h = Math.min(height[left], height[right]);
-            int cur = width * h;
-
-            // 러닝 최대값 갱신
-            if (cur > best) best = cur;
-
-            // 개선 가능성이 있는 '낮은 쪽' 포인터 이동
-            if (height[left] < height[right]) {
+            int area = width * minH;
+            maxA = Math.max(maxA, area);
+            if(minH == height[left]){
                 left++;
-            } else {
+            }else{
                 right--;
             }
         }
-        return best;
+        return maxA;
     }
 }
