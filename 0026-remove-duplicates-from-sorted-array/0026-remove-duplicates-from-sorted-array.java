3eeaf2599f56bb73d9d8314ec804964ab22ff4a6
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2026-03-10 14:46:16 +0900

    Time: 2 ms (7.68%), Space: 46.8 MB (61.12%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,21 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        List<Integer> tmp = new ArrayList<>();
-        int cnt =0;
-        for(int i =0; i<nums.length; i++){
-            // if(!tmp.contains(nums[i]) && i == nums.length-1){
-            //     tmp.add(nums[i]);
-            //     nums[cnt] = nums[i];
-            // }
-            if(!tmp.contains(nums[i])){
-                tmp.add(nums[i]);
-                nums[cnt] = nums[i];
-                cnt++;
-            }else{
-                continue;
+        Set<Integer> set = new HashSet<>();
+        int idx = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(set.add(nums[i])){
+                nums[idx] = nums[i];
+                idx++;
             }
         }
-
-        return cnt;
+        return idx;
     }
 }
