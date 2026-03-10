dffc5192f118d21ab8296063438cb23d87299ad8
Tae Hyun Kim <72403590+tae98@users.noreply.github.com>
2026-03-10 21:39:29 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,7 +1,6 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         double max = Integer.MIN_VALUE;
-        int window = 3;
         int total = 0;
         for(int i = 0; i < nums.length; i++){
             if(i < k - 1){
